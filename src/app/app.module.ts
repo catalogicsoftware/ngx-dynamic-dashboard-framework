a70c52b0cb4d8018319c75ce7f5f038abd15b813
Jay Hamilton <jhamilton@catalogicsoftware.com>
2017-08-19 08:45:35 -0400

    Refactor of app.module - removed unnecessary modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,17 @@ import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import {AppComponent} from './app.component';
-import {CommonModule} from '@angular/common';
 import {RoutingModule} from './routing.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {BoardModule} from './board/board.module';
 
 @NgModule({
     imports: [
-        CommonModule,
         BrowserModule,
         BrowserAnimationsModule,
         RoutingModule,
         FormsModule,
         HttpModule,
-        ReactiveFormsModule,
         BoardModule
 
     ],

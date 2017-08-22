ce5ac4b803b91f63ced6c43774123f34559668f9
Jay Hamilton <jhamilton@catalogicsoftware.com>
2017-08-21 22:09:03 -0400

    Notification implementation

--- src/app/notification/notificationDetail.component.ts
+++ src/app/notification/notificationDetail.component.ts
@@ -18,8 +18,18 @@ export class NotificationDetailComponent {
     constructor() {
 
         this.notifications = [];
-        this.notifications.push(new Notification('CPU Gadget', 'this is a cpu gadget error'));
-        this.notifications.push(new Notification('Trend Gadget', 'this is a trend gadget error2'));
+        let notification = new Notification('CPU Gadget', 'this is a cpu gadget error');
+        notification.setWhen((new Date()).toString());
+        this.notifications.push(notification);
+        notification = new Notification ('Trend Gadget', 'this is a trend gadget error2');
+        notification.setWhen((new Date()).toString());
+        this.notifications.push(notification);
+        notification = new Notification ('Disk Gadget', 'Disk 1 alert');
+        notification.setWhen((new Date()).toString());
+        this.notifications.push(notification);
+        notification = new Notification ('Disk Gadget', 'Disk 2 alert');
+        notification.setWhen((new Date()).toString());
+        this.notifications.push(notification);
 
     }
 

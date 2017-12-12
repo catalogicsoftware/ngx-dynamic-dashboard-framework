c313960503c55855cdf40d565ae07feeff6b7f6e
Jay Hamilton <jhamilton@catalogicsoftware.com>
2017-12-12 08:41:09 -0500

    Added drag and drop to the gadget module.

--- src/app/gadgets/_common/gadget-shared.module.ts
+++ src/app/gadgets/_common/gadget-shared.module.ts
@@ -4,10 +4,12 @@ import {GadgetHeaderComponent} from './gadget-header-component';
 import {GadgetOperationComponent} from './gadget-operation-control-component';
 import {HelpModalComponent} from './help-modal-component';
 import {VisDrillDownComponent} from './vis-drill-down-component';
+import {DndModule} from 'ng2-dnd';
 
 @NgModule({
     imports: [
-        CommonModule
+        CommonModule,
+        DndModule.forRoot(),
     ],
     declarations: [
 

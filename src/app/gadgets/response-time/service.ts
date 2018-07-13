90638bfd0742f9ec9d0527bf229253e511a2180f
jhamilton <jhamilton@catalogicsoftware.com>
2018-07-13 08:55:17 -0400

    Continued refactoring of the Connection Test gadget service in anticipation of multiple port support for the test.

--- src/app/gadgets/response-time/service.ts
+++ src/app/gadgets/response-time/service.ts
@@ -5,6 +5,7 @@ import {Injectable} from '@angular/core';
 import {RuntimeService} from '../../services/runtime.service';
 import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import {ServiceModel} from "./service.model";
+import {environment} from "../../../environments/environment";
 
 @Injectable()
 export class ConnectionService {
@@ -12,6 +13,33 @@ export class ConnectionService {
     connectivityTestURL: string;
 
     constructor(private _http: HttpClient) {
+        this.configure();
+    }
+
+    /**
+     * TODO - the connectivityTestURL should be based on the Endpoint object that currently
+     * comes from the board configuration. However, in order for this to be more modular
+     * the Endpoint object should be managed by the gadget config form. The URL, when run in production mode, is
+     * currently assumes to be the microservice jar file that runs the framework. The GUI should allow
+     * the user to define whether the URL is internal or remote.
+     */
+    configure() {
+
+        if (environment.production) {
+            /**
+             * todo - consider setting this value if a user checks and option in the gadget's form
+             * that indicate the user elects to use the interal api call.
+             */
+
+            this.connectivityTestURL = '/connectTest';
+        } else {
+            /**
+             * todo - consider sett this from the gadget form
+             */
+
+            this.connectivityTestURL = 'http://localhost:8080/connectTest';
+        }
+
     }
 
     get() {

d73595ed35ef1c9bbd9863be03cd611d8a40fe31
jhamilton <jhamilton@catalogicsoftware.com>
2018-08-05 18:10:29 -0400

    Added a toast message when a change occurs in the options page.

--- src/app/configuration/tab-options/options-configuration-tab.component.ts
+++ src/app/configuration/tab-options/options-configuration-tab.component.ts
@@ -5,6 +5,7 @@ import {
     Component
 } from '@angular/core';
 import {OptionsService} from "./service";
+import {ToastService} from "../../toast/toast.service";
 
 @Component({
     selector: 'app-options-config-tab',
@@ -16,14 +17,15 @@ export class OptionsConfigurationTabComponent {
 
     enableHover: boolean;
 
-    constructor(private _optionsService: OptionsService) {
+    constructor(private _optionsService: OptionsService, private _toastService: ToastService) {
 
         this.enableHover = this._optionsService.getBoardOptions()['enableHover'];
     }
 
     onHooverOptionChange(value) {
 
-        this._optionsService.setBoardOptions({"enableHover": value['checked']})
+        this._optionsService.setBoardOptions({"enableHover": value['checked']});
+        this._toastService.sendMessage("The board configuration has changed!",null);
 
 
     }
